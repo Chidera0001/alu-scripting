a52882c7c70df5c3910fb800a67e06e05007ebca
Chidera0001 <c.anele@alustudent.com>
2023-11-11 15:40:11 +0000

    tasks

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-""" 2-recurse.py """
+'''Defines recursive function to return hot posts in subreddit
+'''
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ returns list with titles of all hot articles in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after}
-    response = requests.get(
-                                url,
-                                headers=headers,
-                                params=params,
-                                allow_redirects=False
-                            )
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data is not None:
-            children = data.get('children')
-            if children is not None:
-                for child in children:
-                    hot_list.append(child.get('data').get('title'))
-                after = data.get('after')
-                if after is not None:
-                    return recurse(subreddit, hot_list, after)
-                else:
-                    return hot_list
-        else:
-            return hot_list
-    else:
+def recurse(subreddit, hot_list=[], fullname=None, count=0):
+    '''fetches all hot posts in a subreddit
+
+    Return:
+        None - if subreddit is invalid
+    '''
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    params = {'after': fullname, 'limit': 100, 'count': count}
+    headers = {'user-agent': 'Mozilla/5.0 \
+(Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+    info = requests.get(url, headers=headers,
+                        params=params, allow_redirects=False)
+    if (info.status_code % 400) < 100:
         return None
+    info_json = info.json()
+    results = info_json.get('data').get('children')
+    new_packet = [post.get('data').get('title') for post in results]
+    hot_list += new_packet
+    after = info_json.get('data').get('after', None)
+    dist = info_json.get('data').get('dist')
+    count += dist
+    if after:
+        recurse(subreddit, hot_list, after, count)
+    return hot_list
